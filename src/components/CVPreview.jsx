9ec7c37bd1ac032ce12a9d741d2d9b0443857b43
Jormaedes Luis <jormahed@gmail.com>
2026-04-29 11:27:43 +0100

    feat: add CVPreview
    This components is responsable for displaying all object's data

---
+++ src/components/CVPreview.jsx
@@ -0,0 +1,19 @@
+import PreviewHeader from './preview/PreviewHeader.jsx';
+import PreviewAbout from './preview/PreviewAbout.jsx';
+import PreviewEducation from './preview/PreviewEducation.jsx';
+import PreviewExperience from './preview/PreviewExperience.jsx';
+
+function CVPreview({ cvData }) {
+	return (
+		<div className="cv-preview">
+			<div className="cv-document">
+				<PreviewHeader cvData={cvData} />
+				<PreviewAbout about={cvData.about} />
+				<PreviewEducation education={cvData.education} />
+				<PreviewExperience experiences={cvData.experiences} />
+			</div>
+		</div>
+	);
+}
+
+export default CVPreview;
